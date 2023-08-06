b8ef4755c78f3a3f8c818eb14108661732aa7caf
mohwildan <mobafreewtf@gmail.com>
2023-08-06 17:13:03 +0700

    update

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "apa kata mu mus ti",
+			"data": map[string]any{
+				"name":  "wildan",
+				"email": "mohwildanwildan15@gmail.com",
+			},
 		})
 	})
 	r.Run(":3002") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
