3fc98a40fd1e8db31cd4963fc1e2c9f615c639e8
mohwildan <mobafreewtf@gmail.com>
2023-08-08 22:18:53 +0700

    update

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "apa kata mu mus ti lorem ipsum dolor si amet",
+			"message": "apa kata mu mus ti lorem ipsum dolor si amet mantap",
 			"data": map[string]any{
 				"name":  "wildan",
 				"email": "mohwildanwildan15@gmail.com",
